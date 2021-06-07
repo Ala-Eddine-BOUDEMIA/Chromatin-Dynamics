ba2a06a78833aa46c485171c4a0fe3e66ed804fe
Ala-Eddine-BOUDEMIA <alaeddine.boudemia@etud.univ-evry.fr>
2021-06-07 16:22:50 +0200

    minor changes

--- test_scripts/init_data.R
+++ test_scripts/init_data.R
@@ -3,3 +3,5 @@ tcga = load("/Users/labo/Documents/Data/rse_gene_TCGA.Rdata")
 gtex = load("/Users/labo/Documents/Data/rse_gene_GTEx.Rdata")
 tcga_counts = assays(read_counts(tcga, TRUE, TRUE))$counts
 gtex_counts = assays(read_counts(gtex, TRUE, TRUE))$counts
+write.table(tcga_counts, file="/Users/labo/Documents/Data/tcga/PairedEndRounded.tsv", sep="\t")
+write.table(gtex_counts, file="/Users/labo/Documents/Data/gtex/PairedEndRounded.tsv", sep="\t")
