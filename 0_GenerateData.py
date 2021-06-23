7ce52b45a58e04bf8d9c6982c94bc0e6b4a285ef
Ala-Eddine-BOUDEMIA <alaeddine.boudemia@etud.univ-evry.fr>
2021-06-23 10:26:31 +0200

    generate data to analyze

---
+++ 0_GenerateData.py
@@ -0,0 +1,56 @@
+import pandas as pd 
+from numpy.random import default_rng
+
+from pathlib import Path
+
+import Config
+
+def generate_data(normalized_counts, top1000, 
+	rand, cv, meta, cv_list):
+	
+	counts = pd.read_csv(normalized_counts,
+		header = 0, index_col = 0, sep = "\t")
+
+	chaperones_variants = pd.read_csv(cv_list,
+		header = 0, index_col = "EnsemblGeneId", sep = "\t")
+
+	# Generate the top1000 expressed genes
+	counts["total"] = counts.sum(axis = 0)
+	counts = counts.sort_values("total")
+	counts.pop("total")
+
+	top = counts.iloc[:1000,:]
+	top.to_csv(top1000, sep = "\t")
+
+	# Generate chaperones and variants dataframe
+	for i in counts.index.to_list():
+		counts.rename(index = {i:i.split('.')[0]}, inplace=True)
+
+	cv_df = pd.DataFrame(columns = counts.columns)
+	
+	for i in counts.index.to_list():
+		for j in chaperones_variants.index.to_list():
+			if i.strip() == j.strip():
+				cv_df = cv_df.append(counts.loc[i])
+
+	cv_df.to_csv(cv, sep = "\t")
+
+	# Generate random sets
+	for c in range(10):
+		rng = default_rng()
+		r = rng.choice(len(counts), size=76, replace=False)
+		df_random = pd.DataFrame(columns=counts.columns)
+		
+		for i in r:
+			df_random = df_random.append(counts.iloc[i])
+
+		df_random.to_csv(rand.joinpath(str(c) + ".tsv"), sep="\t")
+
+if __name__ == '__main__':
+	generate_data(
+		normalized_counts = Config.args.norm,
+		top1000 = Config.args.top1000,
+		rand = Config.args.rand,
+		cv = Config.args.cv,
+		meta = Config.args.meta,
+		cv_list = Config.args.list)
