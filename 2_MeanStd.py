63e6b78f33ac2175c2fdef36790b12de9de7a970
Ala-Eddine-BOUDEMIA <alaeddine.boudemia@etud.univ-evry.fr>
2021-06-23 15:31:30 +0200

    minor changes, confirmed no bugs

--- 2_MeanStd.py
+++ 2_MeanStd.py
@@ -2,7 +2,6 @@ import numpy as np
 import pandas as pd
 import plotly.express as px
 
-import Tools
 import Config
 
 def mean_std(
@@ -11,15 +10,16 @@ def mean_std(
 	counts_norm_mv_p, rand_p, top1000_mv_p, cv_mv_p, raw_mv_p, filtered_mv_p):
 	
 	counts = [raw_counts, filtered_counts, counts_norm, top1000, cv_counts]
-	rand_files = Tools.parse_dir(rand)
-	counts.append(rand_files)
+	rand_files = sorted([f for f in rand.iterdir() if f.is_file()])
+	for path in rand_files:
+		counts.append(path)
 
 	images = [raw_mv_i, filtered_mv_i, counts_norm_mv_image, top1000_mv_image, cv_mv_image]
 	htmls = [raw_mv_p, filtered_mv_p, counts_norm_mv_p, top1000_mv_p, cv_mv_p]
 
 	for i in range(len(rand_files)):
 		images.append(rand_images.joinpath("random" + str(i) + ".png"))
-		htmls.append(rand_p.joinpath("random" + str(i)) + ".html")
+		htmls.append(rand_p.joinpath("random" + str(i) + ".html"))
 
 	for file, image, html in zip(counts, images, htmls):
 		f = pd.read_csv(file, header = 0, index_col = 0, sep = "\t")
@@ -37,7 +37,7 @@ def mean_std(
 			title = "Mean Variance Plot")
 
 		fig.write_html(str(html))
-		fig.write_image(str(image))
+		fig.write_image(str(image), width = 2048, height = 1024)
 		fig.show()
 
 if __name__ == '__main__':
