d713944c9b042a83ae7eef2fbcce01731f7b7a5f
Ala-Eddine-BOUDEMIA <alaeddine.boudemia@etud.univ-evry.fr>
2021-06-23 12:36:43 +0200

    minor changes

--- 2_MeanStd.py
+++ 2_MeanStd.py
@@ -2,18 +2,24 @@ import numpy as np
 import pandas as pd
 import plotly.express as px
 
+import Tools
 import Config
 
 def mean_std(
-	counts_norm, top1000, cv_counts, #rand,
-	counts_norm_mv_image, #rand_images,
-	top1000_mv_image, cv_mv_image, counts_norm_mv_p,
-	#rand_p,
-	top1000_mv_p, cv_mv_p):
+	raw_counts, filtered_counts, counts_norm, top1000, cv_counts, rand, raw_mv_i, 
+	filtered_mv_i, counts_norm_mv_image, rand_images, top1000_mv_image, cv_mv_image, 
+	counts_norm_mv_p, rand_p, top1000_mv_p, cv_mv_p, raw_mv_p, filtered_mv_p):
 	
-	counts = [counts_norm, top1000, cv_counts]
-	images = [counts_norm_mv_image, top1000_mv_image, cv_mv_image]
-	htmls = [counts_norm_mv_p, top1000_mv_p, cv_mv_p]
+	counts = [raw_counts, filtered_counts, counts_norm, top1000, cv_counts]
+	rand_files = Tools.parse_dir(rand)
+	counts.append(rand_files)
+
+	images = [raw_mv_i, filtered_mv_i, counts_norm_mv_image, top1000_mv_image, cv_mv_image]
+	htmls = [raw_mv_p, filtered_mv_p, counts_norm_mv_p, top1000_mv_p, cv_mv_p]
+
+	for i in range(len(rand_files)):
+		images.append(rand_images.joinpath("random" + str(i) + ".png"))
+		htmls.append(rand_p.joinpath("random" + str(i)) + ".html")
 
 	for file, image, html in zip(counts, images, htmls):
 		f = pd.read_csv(file, header = 0, index_col = 0, sep = "\t")
@@ -35,16 +41,23 @@ def mean_std(
 		fig.show()
 
 if __name__ == '__main__':
+	
 	mean_std(
+		raw_counts = Config.args.bf,
+		filtered_counts = Config.args.af,
 		counts_norm = Config.args.norm,
 		top1000 = Config.args.top1000,
 		cv_counts = Config.args.cv,
-		#rand = Config.args.rand,
+		rand = Config.args.rand,
+		raw_mv_i = Config.args.ImvRaw,
+		filtered_mv_i = Config.args.ImvFiltered,
 		counts_norm_mv_image = Config.args.ImvNorm,
-		#rand_images = Config.args.ImvRand,
+		rand_images = Config.args.ImvRand,
 		top1000_mv_image = Config.args.ImvNorm,
 		cv_mv_image = Config.args.ImvCV,
 		counts_norm_mv_p = Config.args.PmvNorm,
-		#rand_p = Config.args.PmvRand,
+		rand_p = Config.args.PmvRand,
 		top1000_mv_p = Config.args.PmvNorm,
-		cv_mv_p = Config.args.PmvCV)
+		cv_mv_p = Config.args.PmvCV,
+		raw_mv_p = Config.args.PmvRaw,
+		filtered_mv_p = Config.args.PmvFiltered)
