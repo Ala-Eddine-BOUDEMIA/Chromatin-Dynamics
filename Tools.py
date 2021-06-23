945d4a67ca0fe894217364a9950d6083cc5c67cf
Ala-Eddine-BOUDEMIA <alaeddine.boudemia@etud.univ-evry.fr>
2021-06-23 16:13:13 +0200

    deleted parse_dir

--- Tools.py
+++ Tools.py
@@ -8,17 +8,3 @@ def create_folder(output_folder):
 		os.makedirs(output_folder)
 
 	return output_folder
-
-def parse_dir(input_directories, forme  = ".tsv"):
-
-	path_list = sorted([d for d in input_directories.iterdir() if d.is_dir()])
-	paths = []
-
-	for i in range(len(path_list)):
-		input_directorie = path_list[i]
-		path = sorted(list(input_directorie.glob('**/*.' + forme)))
-		
-		for j in range(len(path)):
-			paths.append(path[j])
-
-	return(path_list, paths)
