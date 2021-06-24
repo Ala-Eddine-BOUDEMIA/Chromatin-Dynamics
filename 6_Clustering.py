48424f85c4091287f591890cc915d6600bd62803
Ala-Eddine-BOUDEMIA <alaeddine-boudemia@hotmail.com>
2021-06-24 16:15:36 +0200

    added clustering samples by genes

--- 6_Clustering.py
+++ 6_Clustering.py
@@ -1,6 +1,7 @@
 import sys
 import pandas as pd
 import seaborn as  sns
+import matplotlib.pyplot as plt
 
 from matplotlib.patches import Patch
 
@@ -47,7 +48,8 @@ def clustering_samples(
         
         handles = [Patch(facecolor = lut[name]) for name in lut]
         g.ax_row_dendrogram.legend(handles, lut, title = 'Tissues',
-            bbox_to_anchor = (0, 1), loc='best')
+            bbox_to_anchor = (0, 1), loc = 'best', 
+            bbox_transform = plt.gcf().transFigure)
 
         g.savefig(str(i), dpi = 300)
 
@@ -70,7 +72,7 @@ def clustering_genes(
     for m, i in zip(corr_matices, images):
         f = pd.read_csv(m, header = 0, index_col = 0, sep = '\t')
 
-        if c < 3:
+        if c == 2:
             correlation_matrix = f.join(metadata["Class"])
             correlation_matrix = f.join(metadata["GeneName"])
             correlation_matrix = correlation_matrix.dropna()
@@ -97,10 +99,84 @@ def clustering_genes(
         
         handles = [Patch(facecolor = lut[name]) for name in lut]
         g.ax_row_dendrogram.legend(handles, lut, title = 'Class',
-            bbox_to_anchor = (0, 1), loc='best')
+            bbox_to_anchor = (0, 1), loc = 'best', 
+            bbox_transform = plt.gcf().transFigure)
 
         g.savefig(str(i), dpi = 300)
         c += 1
+
+def clustering_samples_genes(
+    meta, cv_list, 
+    counts_norm, top1000, cv_counts, rand, 
+    sg_img_clstrRand, sg_img_clstrNorm, 
+    sg_img_clstrTop, sg_img_clstrCv):
+    
+    counts = [counts_norm, top1000, cv_counts] 
+    rand_files = sorted([f for f in g_corr_rand.iterdir() if f.is_file()])
+    for path in rand_files:
+        corr_matices.append(path)
+
+    images = [sg_img_clstrNorm, sg_img_clstrTop, sg_img_clstrCv]
+    for i in range(len(rand_files)):
+        images.append(g_img_clstrRand.joinpath("random" + str(i) + ".png"))  
+
+    metadata = pd.read_csv(meta, header = 0, index_col = 0, sep = "\t")
+    cv_list = pd.read_csv(cv_list, header = 0, index_col = 0, sep = "\t") 
+    
+    c = 0
+    for m, i in zip(counts, images):
+        f = pd.read_csv(m, header = 0, index_col = 0, sep = '\t')
+        f = pd.DataFrame(np.log2(f + 1))
+
+        count = f.T.join(metadata["smts"])
+        count = count.dropna()
+        tissues = count.pop("smts")
+
+        palette1 = sns.hls_palette(10)
+        palette2 = sns.color_palette("bwr",10)
+        palette3 = sns.color_palette("inferno",10)
+        palette = palette1 + palette2 + palette3
+        lut = dict(zip(set(tissues.unique()), palette))
+        col_colors = tissues.map(lut)
+
+        if c == 2:
+            count = count.T
+            count = count.join(cv_list["GeneName"])
+            count = count.join(cv_list["Class"])
+            classes = count.pop("Class")
+            genes = c.pop("GeneName")
+
+            paletteX = sns.color_palette("Set2" ,len(classes))
+            lutX = dict(zip(set(classes.unique()), paletteX))
+            row_colors = classes.map(lutX)
+
+            yticklabels = genes
+
+        else:
+            row_colors, yticklabels = False, False
+
+        g = sns.clustermap(count, 
+            vmin = max(count.max(axis = 1)), 
+            vmax = min(count.min(axis = 1)), 
+            row_colors = row_colors,
+            col_colors = col_colors,
+            cmap = "icefire",
+            xticklabels = False, 
+            yticklabels = yticklabels,
+            method = "weighted",
+            figsize = [15, 15])
+
+        handles = [Patch(facecolor = lut[name]) for name in lut]
+        g.ax_col_dendrogram.legend(handles, lut, title = 'Tissues',
+            bbox_to_anchor = (0, 1), loc = 'best', 
+            bbox_transform = plt.gcf().transFigure)
+
+        handlesX = [Patch(facecolor = lutX[name]) for name in lutX]
+        g.ax_row_dendrogram.legend(handlesX, lutX, title = 'Class',
+            bbox_to_anchor = (0, 0), loc = 'best', 
+            bbox_transform = plt.gcf().transFigure)
+
+        g.savefig(str(i), dpi = 300)
 
 if __name__ == '__main__':
 
@@ -125,3 +201,13 @@ if __name__ == '__main__':
         g_img_clstrNorm = Config.args.IclusterNormG,
         g_img_clstrTop = Config.args.IclusterTopG,
         g_img_clstrCv = Config.args.IclusterCVG)
+
+    clustering_samples_genes(
+        counts_norm = Config.args.norm,
+        top1000 = Config.args.top1000,
+        cv_counts = Config.args.cv,
+        rand = Config.args.rand,
+        sg_img_clstrRand = Config.args.IclusterRandSG,
+        sg_img_clstrNorm = Config.args.IclusterNormSG,
+        sg_img_clstrTop = Config.args.IclusterTopSG,
+        sg_img_clstrCv = Config.args.IclusterCVsg)
