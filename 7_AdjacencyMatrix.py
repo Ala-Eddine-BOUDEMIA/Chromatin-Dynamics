4cddf3ef842a9cf51b3de8fce932de2363110454
Ala-Eddine-BOUDEMIA <alaeddine.boudemia@etud.univ-evry.fr>
2021-06-25 12:27:34 +0200

    just saving for now

--- 7_AdjacencyMatrix.py
+++ 7_AdjacencyMatrix.py
@@ -2,44 +2,48 @@ import numpy as np
 import pandas as pd
 import networkx as nx
 import matplotlib.pyplot as plt
-import plotly.graph_objects as go
 
 import Config
 
 pd.set_option('display.max_rows', None)
-pd.set_option('display.max_columns', 10)
+pd.set_option('display.max_columns', None)
 pd.set_option('display.width', None)
-pd.set_option('display.max_colwidth', 1)
+pd.set_option('display.max_colwidth', None)
 
 def adjacencyMatrix(
-	meta, cv_list, g_corr_cv):
+	cv_list, g_corr_cv):
 
 	correlation_matrix = pd.read_csv(g_corr_cv, 
 		header = 0, index_col = 0, sep = '\t')
 
 	cv_list = pd.read_csv(cv_list, 
-		header = 0, index_col = 0, sep = '\t')
+		header = 0, index_col = 0, sep = ';')
+	
+	for i in range(len(correlation_matrix.index)):
+		for j in range(len(correlation_matrix.columns)):
+			if i == j:
+				correlation_matrix.iloc[i, j] = 0
 
 	correlation_matrix = correlation_matrix.join(cv_list["GeneName"])
 	labels = correlation_matrix["GeneName"]
-
+	
 	adjacency_matrix = pd.DataFrame(np.where(
 		correlation_matrix.iloc[:, correlation_matrix.columns != "GeneName"] > 0.7, 1, 0), 
 		index = labels,
 		columns = labels)
-
+	
 	G = nx.from_pandas_adjacency(adjacency_matrix)
+	print(nx.info(G))
 	G.name = "Graph from pandas adjacency matrix"
-	nx.draw_kamada_kawai(G, 
+	#pos=nx.graphviz_layout(G,prog='dot')
+	nx.draw_random(G, 
 		with_labels = True, 
 		edge_cmap = "inferno")
 	
 	plt.show()
 
-
 if __name__ == '__main__':
 
 	adjacencyMatrix(
-		meta = Config.args.meta,
 		cv_list = Config.args.list,
 		g_corr_cv = Config.args.corrCVg)
