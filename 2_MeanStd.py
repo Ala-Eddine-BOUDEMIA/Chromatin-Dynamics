ad0d388f618d52f729cc2548c20c4ac4797ff6e9
Ala-Eddine-BOUDEMIA <alaeddine.boudemia@etud.univ-evry.fr>
2021-07-05 18:02:44 +0200

    mv plots

--- 2_MeanStd.py
+++ 2_MeanStd.py
@@ -42,20 +42,20 @@ def mean_std(
 		Tools.create_folder(link_img)
 		Tools.create_folder(link_p)
 
-		images.append(link_img.joinpath("/" + tissue_name + ".png"))
-		htmls.append(link_p.joinpath("/" + tissue_name + ".html"))
+		images.append(link_img.joinpath(tissue_name + ".png"))
+		htmls.append(link_p.joinpath(tissue_name + ".html"))
 
 	for file, image, html in zip(counts, images, htmls):
 		f = pd.read_csv(file, header = 0, index_col = 0, sep = "\t")
 
-		mean = np.log2(f + 0.5).mean(axis = 1)
+		mean = np.log2(f + 1).mean(axis = 1)
 		std = np.sqrt(f).std(axis = 1)
 		meanStd = pd.DataFrame(data=[mean, std], 
-			index=['log2(mean+0.5)','sqrt(std)']).T
+			index=['log2(mean+1)','sqrt(std)']).T
 
 		fig = px.scatter(
 			data_frame = meanStd, 
-			x = "log2(mean+0.5)", 
+			x = "log2(mean+1)", 
 			y = "sqrt(std)",
 			hover_data = [meanStd.index],
 			title = "Mean Variance Plot")
--- 3_PCA.py
+++ 3_PCA.py
@@ -9,20 +9,47 @@ import Tools
 import Config
 
 def pca(
-    meta, raw_counts, filtered_counts, counts_norm, top1000, cv_counts, rand, 
-    file_pcaRaw, file_pcaFiltered, file_pca_rand, file_pcaNorm, file_pcaTop, 
-    file_pcaCV, img_pca_raw, img_pca_filtered, img_pca_rand, img_pca_norm, 
-    img_pca_top, img_pca_cv, p_pca_raw, p_pca_filtered, p_pca_rand, p_pca_norm, 
-    p_pca_top, p_pca_cv):
+    meta, rand, top88, cv_counts, raw_counts, top1000, 
+    counts_norm, filtered_counts, tissue_counts, p_pca_cv,
+    file_pcaCV, img_pca_cv, p_pca_raw, file_pcaRaw, img_pca_raw, 
+    p_pca_top, file_pcaTop, img_pca_top, p_pca_norm, file_pcaNorm, 
+    img_pca_norm, p_pca_rand, file_pca_rand, img_pca_rand, p_pca_top88, 
+    file_pcaTop88, img_pca_top88, file_pca_tissue, p_pca_tissue, 
+    img_pca_tissues, p_pca_filtered, file_pcaFiltered, img_pca_filtered):
     
-    counts = [raw_counts, filtered_counts, counts_norm, top1000, cv_counts]
+    counts = [raw_counts, filtered_counts, counts_norm, 
+        top1000, top88, cv_counts]
+
+    tissue_files = sorted([f for f in tissue_counts.iterdir() if f.is_file()])
+    for path in tissue_files:
+        counts.append(path)
+
     rand_files = sorted([f for f in rand.iterdir() if f.is_file()])
     for path in rand_files:
         counts.append(path)
 
-    tsvs = [file_pcaRaw, file_pcaFiltered, file_pcaNorm, file_pcaTop, file_pcaCV]
-    images = [img_pca_raw, img_pca_filtered, img_pca_norm, img_pca_top, img_pca_cv]
-    htmls = [p_pca_raw, p_pca_filtered, p_pca_norm, p_pca_top, p_pca_cv]
+    tsvs = [file_pcaRaw, file_pcaFiltered, file_pcaNorm, 
+        file_pcaTop, file_pcaTop88, file_pcaCV]
+    
+    images = [img_pca_raw, img_pca_filtered, img_pca_norm, 
+        img_pca_top, img_pca_top88, img_pca_cv]
+    
+    htmls = [p_pca_raw, p_pca_filtered, p_pca_norm, 
+        p_pca_top, p_pca_top88, p_pca_cv]
+
+    for i in range(len(tissue_files)):
+        tissue_name = str(tissue_files[i]).split("/")[-1].split(".")[0]
+        link_tsv = file_pca_tissue.joinpath(tissue_name)
+        link_img = img_pca_tissues.joinpath(tissue_name)
+        link_p = p_pca_tissue.joinpath(tissue_name)
+        
+        Tools.create_folder(link_tsv)
+        Tools.create_folder(link_img)
+        Tools.create_folder(link_p)
+        
+        tsvs.append(link_tsv)
+        images.append(link_img)
+        htmls.append(link_p)
 
     for i in range(len(rand_files)):
         link_tsv = file_pca_rand.joinpath("random" + str(i) + "/")
@@ -38,73 +65,66 @@ def pca(
         htmls.append(link_html)
 
     metadata = pd.read_csv(meta, header = 0, index_col = 0, sep = '\t')
-    tissues = metadata["smts"]
     sub_tissues = metadata["smtsd"]
 
-    for c, tsv, i, h in zip(counts, tsvs, images, htmls):
+    for c, t, i, h in zip(counts, tsvs, images, htmls):
         f = pd.read_csv(c, header = 0, index_col = 0, sep = "\t")
         lib_size = f.sum(axis = 0).to_frame(name = "lib_size")
         f = pd.DataFrame(np.log2(f + 1)).T
-        f = f.join(tissues)
 
-        tissue_types = tissues.unique()
-        for t in tissue_types:
-            df = pd.DataFrame(columns = f.columns)
+        scaler = StandardScaler()
+        std_counts = scaler.fit_transform(f.T.dropna())
 
-            for i in f.index:
-                if f.loc[i]["smts"] == t:
-                    df = df.append(f.loc[i])
+        pca = PCA(n_components = 2)
+        P = pca.fit_transform(std_counts)
+        ratio = pca.explained_variance_ratio_ * 100
 
-            df.pop("smts")
+        d = pd.DataFrame(index = f.T.index)
+        d["PC1"] = P[:, 0]
+        d["PC2"] = P[:, 1]
+        d = d.join(lib_size)
+        d = d.join(sub_tissues)
+        d.sort_values("smtsd", inplace = True)
 
-            scaler = StandardScaler()
-            std_counts = scaler.fit_transform(df.dropna())
+        print("PC1: ", round(ratio[0], 2))
+        print("PC2: ", round(ratio[1], 2))
 
-            pca = PCA(n_components = 2)
-            P = pca.fit_transform(std_counts)
-            ratio = pca.explained_variance_ratio_ * 100
+        loading_scores = pd.Series(pca.components_[0], index = f.index)
+        sorted_loading_scores = loading_scores.abs().sort_values(ascending = False)
+        top_10_genes = sorted_loading_scores[0:10].index.values
+        print(loading_scores[top_10_genes])
+        
+        fig = px.scatter(
+            data_frame = d.dropna(), 
+            x = "PC1", y = "PC2",
+            color = "smtsd", 
+            hover_data = [d.dropna().index, "lib_size"],
+            #size = "lib_size",
+            title = "GTEx PCA")
 
-            d = pd.DataFrame(index = df.index)
-            d["PC1"] = P[:, 0]
-            d["PC2"] = P[:, 1]
-            d = d.join(lib_size)
-            d = d.join(sub_tissues)
-            d.sort_values("smtsd", inplace = True)
+        fig.write_html(h.joinpath("pca.html"))
+        fig.write_image(i.joinpath("pca.png"), width = 2048, height = 1024)
+        fig.show()
 
-            print("PC1: ", round(ratio[0], 2))
-            print("PC2: ", round(ratio[1], 2))
-
-            loading_scores = pd.Series(pca.components_[0], index = df.index)
-            sorted_loading_scores = loading_scores.abs().sort_values(ascending = False)
-            top_10_genes = sorted_loading_scores[0:10].index.values
-            print(loading_scores[top_10_genes])
-            
-            fig = px.scatter(
-                data_frame = d.dropna(), 
-                x = "PC1", y = "PC2",
-                color = "smtsd", 
-                hover_data = [d.dropna().index, "lib_size"],
-                #size = "lib_size",
-                title = "GTEx PCA")
-
-            fig.write_html(h.joinpath("pca_" + t + ".html"))
-            fig.write_image(i.joinpath("pca_" + t + ".png"), width = 2048, height = 1024)
-            fig.show()
-
-            d.to_csv(tsv.joinpath("pca_" + t + ".tsv"), sep="\t")
+        d.to_csv(t.joinpath("pca.tsv"), sep="\t")
 
 if __name__ == '__main__':	
     
     pca(
-        meta = Config.args.meta, raw_counts = Config.args.bf,
-		filtered_counts = Config.args.af, counts_norm = Config.args.norm,
-		top1000 = Config.args.top1000, cv_counts = Config.args.cv, rand = Config.args.rand,
-        file_pcaRaw = Config.args.pcaRaw, file_pcaFiltered = Config.args.pcaFiltered,
-        file_pca_rand = Config.args.pcaRand, file_pcaNorm = Config.args.pcaNorm,
-        file_pcaTop = Config.args.pcaTop, file_pcaCV = Config.args.pcaCV,
-        img_pca_raw = Config.args.IpcaRaw, img_pca_filtered = Config.args.IpcaFiltered,
-        img_pca_rand = Config.args.IpcaRand, img_pca_norm = Config.args.IpcaNorm,
-        img_pca_top = Config.args.IpcaTop, img_pca_cv = Config.args.IpcaCV,
-        p_pca_raw = Config.args.PpcaRaw, p_pca_filtered = Config.args.PpcaFiltered,
-        p_pca_rand = Config.args.PpcaRand, p_pca_norm = Config.args.PpcaNorm,
-        p_pca_top = Config.args.PpcaTop, p_pca_cv = Config.args.PpcaCV)
+        meta = Config.args.meta, rand = Config.args.rand,
+        top88 = Config.args.top88, cv_counts = Config.args.cv,
+        raw_counts = Config.args.bf, top1000 = Config.args.top1000, 
+        counts_norm = Config.args.norm, filtered_counts = Config.args.af, 
+        tissue_counts = Config.args.tissue, p_pca_cv = Config.args.PpcaCV,
+        file_pcaCV = Config.args.pcaCV, img_pca_cv = Config.args.IpcaCV,
+        p_pca_raw = Config.args.PpcaRaw, file_pcaRaw = Config.args.pcaRaw, 
+        img_pca_raw = Config.args.IpcaRaw, p_pca_top = Config.args.PpcaTop,
+        file_pcaTop = Config.args.pcaTop, img_pca_top = Config.args.IpcaTop,
+        p_pca_norm = Config.args.PpcaNorm, file_pcaNorm = Config.args.pcaNorm, 
+        img_pca_norm = Config.args.IpcaNorm, p_pca_rand = Config.args.PpcaRand,
+        file_pca_rand = Config.args.pcaRand, img_pca_rand = Config.args.IpcaRand, 
+        p_pca_top88 = Config.args.PpcaTop88, file_pcaTop88 = Config.args.pcaTop88, 
+        img_pca_top88 = Config.args.IpcaTop88, p_pca_tissue = Config.args.PpcaTissue,
+        file_pca_tissues = Config.args.pcaTissue, img_pca_tissues = Config.args.IpcaTissue,
+        p_pca_filtered = Config.args.PpcaFiltered, file_pcaFiltered = Config.args.pcaFiltered,
+        img_pca_filtered = Config.args.IpcaFiltered)
